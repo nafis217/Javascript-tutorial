b4adb1e841a964424d5fee3e18d6f51e1e96ae6b
MD NAFIS AL SAFAYET <nafisalsafayet23@gmail.com>
2025-08-25 11:36:54 +0600

    Add files via upload

---
+++ session.js
@@ -0,0 +1,15 @@
+function setSessionStorage(key, value){
+    sessionStorage.setItem(key, value);
+}
+
+function getSessionStorage(key){
+    alert(sessionStorage.getItem(key));
+}
+
+function removeSessionStorage(key){
+    sessionStorage.removeItem(key);
+}
+
+function clearSessionStorage(){
+    sessionStorage.clear();
+}
